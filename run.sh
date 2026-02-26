32dd75062334de554529dc1cd95418b4e0558910
Daniel Feliks Evangelista <evangelistadanielfeliks@gmail.com>
2026-02-26 20:13:44 +0100

    added different console comands to run in multiple linux terminals

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+if command -v konsole >/dev/null; then
+    TERMINAL="konsole -e"
+elif command -v gnome-terminal >/dev/null; then
+    TERMINAL="gnome-terminal --"
+else
+    TERMINAL="xterm -e"
+fi
 echo "Accensione monitor..."
-konsole  --title "ECU MONITOR" -e "./bin/candump" &
+$TERMINAL "./bin/candump" &
 sleep 1
 echo "Avvio ECU..."
 ./bin/clusterA &
